f4320ba78ea046d9cc4fe9da761ca5b5b13334d6
jovanrt30 <46754013+jovanrt30@users.noreply.github.com>
2019-04-22 14:15:54 +0700

    Add files via upload

---
+++ src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.swing.JFrame;
+
+import panel.GamePanel;
+import panel.MainMenuPanel;
+
+public class Main {
+
+	public static final int WIDTH = 900;
+	public static final int HEIGHT = 600;
+	public static JFrame frame;
+	
+	public Main() {
+		frame = new JFrame("susutaww");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		frame.add(new MainMenuPanel());
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+	
+}
