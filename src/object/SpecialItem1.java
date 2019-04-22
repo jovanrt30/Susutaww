1a1c36b36a95cd87c990d681c47937d9b07f33f2
jovanrt30 <46754013+jovanrt30@users.noreply.github.com>
2019-04-22 14:21:24 +0700

    edit new object

---
+++ src/object/SpecialItem1.java
@@ -0,0 +1,10 @@
+package object;
+
+public class SpecialItem1 extends Item{
+
+	public SpecialItem1() {
+		this.setName("BIG HEALING POTION");
+		this.setEffect(30);
+		this.isValid = true;
+	}
+}
---
+++ src/object/SpecialItem2.java
@@ -0,0 +1,10 @@
+package object;
+
+public class SpecialItem2 extends Item{
+
+	public SpecialItem2() {
+		this.setName("HEALING POTION");
+		this.setEffect(20);
+		this.isValid = true;
+	}
+}
